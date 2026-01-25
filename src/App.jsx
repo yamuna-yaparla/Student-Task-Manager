29a1f517fd912a6f3631e1f20b598cdac4134b0c
yamuna <yamunayaparla@gmail.com>
2026-01-25 21:00:21 +0530

    Day 1: React setup and TaskList component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TaskList from "./components/TaskList";
+
+function App(){
+  return(
+    <div>
+      <h1>Student Task Manager</h1>
+      <TaskList />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+function TaskList()
+{
+    return(
+        <div>
+        <ul>
+            <li>My React app</li>
+            <li>My First day</li>
+            <li>Building app</li>
+        </ul>
+        </div>
+    );
+}
+export default TaskList;
