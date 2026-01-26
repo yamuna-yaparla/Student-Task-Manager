1205edaa66832c4f495ce2ac61e28a7dde205ddb
yamuna <yamunayaparla@gmail.com>
2026-01-26 14:59:03 +0530

    Day 2: Added task input and dynamic task list using useState

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,36 @@
-function TaskList()
-{
-    return(
-        <div>
-        <ul>
-            <li>My React app</li>
-            <li>My First day</li>
-            <li>Building app</li>
-        </ul>
-        </div>
-    );
+import { useState } from "react";
+
+function TaskList() {
+  const [tasks, setTasks] = useState([
+    "Learn React",
+    "Practice DSA",
+    "Build Projects"
+  ]);
+
+  const [newTask , setNewTask]=useState("");
+
+  const addTask = ()=>{
+     if (newTask.trim() === "") return;
+     
+     setTasks([...tasks, newTask]);
+     setNewTask("")
+  };
+
+  return (
+    <div>
+        <input type= "text" 
+         placeholder="Enter new task"
+         value={newTask}
+         onChange={(e)=>setNewTask(e.target.value)} />
+
+         <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default TaskList;
